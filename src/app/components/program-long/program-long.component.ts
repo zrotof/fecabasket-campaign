bd47cb4aaa59174f08b003635a02185ec171f8bb
Nadia Mekou <nadiamekou@nadia.lan>
2022-11-08 04:35:18 +0100

    new features

--- src/app/components/program-long/program-long.component.ts
+++ src/app/components/program-long/program-long.component.ts
@@ -7,9 +7,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgramLongComponent implements OnInit {
 
+  menus!: any[] ;
+  activedMenuIndex: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.menus = this.initMenus();
+  }
+
+  initMenus(){
+    return [
+      {
+        label: "Ressources",
+        opened: true,
+        index: 0
+      },
+      {
+        label: "Infrastructures",
+        opened: false,
+        index: 1
+      },
+      {
+        label: "Commisions et Missions",
+        opened: false,
+        index: 2
+      },
+      {
+        label: "Championnats",
+        opened: false,
+        index: 3
+      },
+      {
+        label: "Marketing",
+        opened: false,
+        index: 4
+      },
+      {
+        label: "Gouvernance",
+        opened: false,
+        index: 5
+      },
+      {
+        label: "Formation",
+        opened: false,
+        index: 6
+      },
+      {
+        label: "Direction Technique Nationale",
+        opened: false,
+        index: 7
+      },
+      {
+        label: "Communication",
+        opened: false,
+        index: 8
+      },
+      {
+        label: "Collaboration Internationale",
+        opened: false,
+        index: 9
+      }
+    ]
+  }
+
+  onActivateMenu(index: number){
+    this.menus.map(menu =>{
+      menu.opened = false
+    })
+
+    this.menus[index].opened = true;
+    this.activedMenuIndex = index;
   }
 
 }
